706a00b94dae19e84c8b41b2299a87605a572b4b
Kristian Haugene <kristian.haugene@capgemini.com>
2014-06-11 11:46:20 -0400

    timing issues. Not that the curl operation should be part of a VM build. Demo purposes

--- vagrant/local-box/provision/runStuff.sh
+++ vagrant/local-box/provision/runStuff.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "Start Docker-Gen to monitor Docker instances"
-cd /git/docker-demo/docker-gen/
-./start.sh &
-sleep 1
-
 echo "Starting nginx"
 service nginx start
 sleep 3
+
+echo "Start Docker-Gen to monitor Docker instances"
+cd /git/docker-demo/docker-gen/
+./start.sh &
+sleep 2
--- vagrant/local-box/provision/warmup.sh
+++ vagrant/local-box/provision/warmup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "Wait 10 sec"
-sleep 10
+echo "Running warmup script in 20 sec"
+sleep 20
 
 echo "Curl to localhost, let's see if someone is serving us"
 cd /git/docker-demo/jetty/
