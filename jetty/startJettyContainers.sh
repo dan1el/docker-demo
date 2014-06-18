a82609f0b2f6bf67db827f2bf4776f46bd57d17e
Kristian Haugene <kristian.haugene@capgemini.com>
2014-06-18 16:08:02 +0200

    Set war-location

--- jetty/startJettyContainers.sh
+++ jetty/startJettyContainers.sh
@@ -6,6 +6,6 @@ echo "Specify the number of containers to run"
 else
 for server in "${servers[@]}"
 do
-    sudo docker run -v $2:/opt/jetty/webapps:rw -h instance-$server.dockerdemo.com -d -e VIRTUAL_HOST=instance-$server.dockerdemo.com dockerdemo/jetty
+    sudo docker run -v /git/simple-webapp-docker/deploy/1.0/:/opt/jetty/webapps:rw -h instance-$server.dockerdemo.com -d -e VIRTUAL_HOST=instance-$server.dockerdemo.com dockerdemo/jetty
 done
 fi
